0e9604edca5de7647787ad1aaf5fba1ac3c703a4
Alex Warstadt <alexwarstadt@gmail.com>
2020-09-30 22:32:05 -0400

    add posterior stimuli and likert skeleton

---
+++ helper_scripts/csv_to_items.py
@@ -0,0 +1,29 @@
+import csv
+
+contexts = ['context-low-bias', 'context-positive-bias', 'context-negative-bias']
+answers = ['answer-high-certainty', 'answer-low-certainty', 'answer-reductive-answer', 'answer-exhaustive-answer', 'answer-non-answer']
+
+
+out_file_lines = []
+
+with open('relevance_stimuli_all.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    item_counter = 1
+    for row in reader:
+        for c in contexts:
+            for a in answers:
+                # setup_posterior = row["setup-posterior"]
+                # answer_text = row[a]
+                out_file_lines.append('# target ' + str(item_counter) + ' ' + c + ' ' + a)
+                out_file_lines.append(row[c])
+                out_file_lines.append(row["setup-posterior"])
+                out_file_lines.append(row[a])
+                out_file_lines.append(row["prompt-likelihood"])
+                out_file_lines.append('')
+        item_counter += 1
+print(item_counter)
+print(out_file_lines)
+
+with open('likelihood-items.txt', 'w') as outfile:
+    for line in out_file_lines:
+        outfile.write('%s\n' % line)
