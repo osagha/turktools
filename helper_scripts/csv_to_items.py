67eb2d33b57e1fd8f5d2a16661c897e215872923
Alex Warstadt <alexwarstadt@gmail.com>
2021-02-12 00:36:11 -0500

    finalize stimuli and begin results processing for pilot

--- helper_scripts/csv_to_items.py
+++ helper_scripts/csv_to_items.py
@@ -51,6 +51,34 @@ with open('stimuli/posterior-items.txt', 'w') as outfile:
         outfile.write('%s\n' % line)
 
 
+# create helpfulness items text file
+out_file_lines = []
+with open('stimuli/relevance_stimuli_all.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    item_counter = 1
+    for row in reader:
+        for c in contexts:
+            for a in answers:
+                out_file_lines.append('# target ' + str(item_counter) + ' ' + c + ' ' + a)
+                out_file_lines.append(row[c])
+                out_file_lines.append(row["setup-relevance-your-turn"])
+                out_file_lines.append(row["Question"])
+                out_file_lines.append(row["name"] + " responds:")
+                out_file_lines.append(row[a])
+                out_file_lines.append(row["prompt-helpfulness"].format(name=row["name"]))
+                out_file_lines.append('')
+        item_counter += 1
+
+with open('stimuli/helpfulness-items.txt', 'w') as outfile:
+    with open('stimuli/helpfulness-fillers.txt', 'r') as fillers:
+        for line in fillers:
+            outfile.write(line)
+    outfile.write("\n")
+    for line in out_file_lines:
+        outfile.write('%s\n' % line)
+
+
+
 # create relevance items text file
 out_file_lines = []
 with open('stimuli/relevance_stimuli_all.csv') as csvfile:
@@ -65,7 +93,7 @@ with open('stimuli/relevance_stimuli_all.csv') as csvfile:
                 out_file_lines.append(row["Question"])
                 out_file_lines.append(row["name"] + " responds:")
                 out_file_lines.append(row[a])
-                out_file_lines.append(row["prompt-helpfulness"])
+                out_file_lines.append(row["prompt-relevance"].format(name=row["name"]))
                 out_file_lines.append('')
         item_counter += 1
 
--- results/results_processing.py
+++ results/results_processing.py
@@ -1,12 +1,16 @@
 import csv
 import pandas as pd
 import numpy as np
+import os
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-with open('prior_10_15.csv') as f:
-    data = [{k: v for k, v in row.items()}
-        for row in csv.DictReader(f, skipinitialspace=True)]
 
-# df = pd.read_csv("prior_10_8.csv", header=0)
+
+results_dir = "pilot_11_Feb_21"
+prior = os.path.join(results_dir, "prior.csv")
+posterior = os.path.join(results_dir, "posterior.csv")
+helpfulness = os.path.join(results_dir, "helpfulness.csv")
 
 def LLO(p):
     if p == 0:
@@ -14,63 +18,48 @@ def LLO(p):
     else:
         return 1 / (1 + ((-1 + 1 / p) ** 1.5))
 
-# for column in df:
-#     if column.startswith("Answer.response"):
-#         df[column] = df[column].apply(LLO)
-#
-data_columns = ["WorkerId", "Input.list"]
-item_columns = ["Answer.response%d", "Input.item_%d_condition"]
-# data_columns.extend(["Answer.response%d" % i for i in range(1, 15)])
-# data_columns.extend(["Input.item_%d_condition" % i for i in range(1, 15)])
-#
-# responses = df[data_columns]
+def create_df(file, experiment):
+    with open(file) as f:
+        data = [{k: v for k, v in row.items()}
+            for row in csv.DictReader(f, skipinitialspace=True)]
 
-responses = []
-for worker in data:
-    new = {}
-    for k in worker:
-        if k in data_columns:
-            new[k] = worker[k]
-        new["items"] = []
+    data_columns = ["WorkerId", "Input.list"]
+    responses = []
+    for worker in data:
         for i in range(1,15):
             item_dic = {
                 "item_number": worker["Input.item_%d_number" % i],
                 "condition": worker["Input.item_%d_condition" % i],
                 "response": LLO(float(worker["Answer.response%d" % i])),
+                "slider_position": float(worker["Answer.response%d" % i]),
                 "context": worker["Input.field_%d_1" % i],
-                "prompt": worker["Input.field_%d_2" % i],
+                "answer": None if experiment=="prior" else worker["Input.field_%d_3" % i] if experiment=="posterior" else worker["Input.field_%d_5" % i],
             }
-            new["items"].append(item_dic)
-    responses.append(new)
+            for k in worker:
+                if k in data_columns:
+                    item_dic[k] = worker[k]
+            responses.append(item_dic)
+    df = pd.DataFrame(responses)
+    return df
 
-# for worker in responses:
-#     for k in worker:
-#         if k.startswith("Answer.response"):
-#             worker[k] = LLO(float(worker[k]))
+df_prior = create_df(prior, "prior")
+df_prior["condition_context"] = df_prior["condition"].apply(lambda x: x if x == "filler" else x[8:])
+df_prior["condition_answer"] = float("nan")
+df_prior["experiment"] = "prior"
 
-# new_responses = []
-# for worker in responses:
-#     items = []
-#     for i in range(1, 15):
-#         condition = worker["Input.item_%d_condition" % i]
-#         response = worker["Answer.response%d" % i]
-#         items.append({"condition": condition, "response": response})
-#     new_responses.append({"WorkerId": worker["WorkerId"],
-#                           "Input.list": worker["Input.list"],
-#                           "Responses": items
-#                           })
+df_posterior = create_df(posterior, "posterior")
+df_posterior["condition_context"] = df_posterior["condition"].apply(lambda x: x if x == "filler" else x.split()[0][8:])
+df_posterior["condition_answer"] = df_posterior["condition"].apply(lambda x: x if x == "filler" else x.split(" ")[1][7:])
+df_posterior["experiment"] = "posterior"
 
+df_helpfulness = create_df(helpfulness, "helpfulness")
+df_helpfulness["condition_context"] = df_helpfulness["condition"].apply(lambda x: x if x == "filler" else x.split()[0][8:])
+df_helpfulness["condition_answer"] = df_helpfulness["condition"].apply(lambda x: x if x == "filler" else x.split()[1][7:])
+df_helpfulness["experiment"] = "helpfulness"
 
-# responses = new_responses
-print("\t".join(["WorkerId", "Input.list", "negative_bias_average", "low_bias_average", "positive_bias_average"]))
-for worker in responses:
-    low_bias_average = np.mean([r["response"] for r in worker["items"] if r["condition"] == "low-bias"])
-    positive_bias_average = np.mean([r["response"] for r in worker["items"] if r["condition"] == "positive-bias"])
-    negative_bias_average = np.mean([r["response"] for r in worker["items"] if r["condition"] == "negative-bias"])
-    worker["low_bias_average"] = low_bias_average
-    worker["positive_bias_average"] = positive_bias_average
-    worker["negative_bias_average"] = negative_bias_average
-    print("\t".join([worker["WorkerId"], worker["Input.list"], str(negative_bias_average), str(low_bias_average), str(positive_bias_average)]))
+df = pd.concat([df_helpfulness, df_prior, df_posterior])
+# print(df_helpfulness.to_string())
+
 
 def passed_fillers(items):
     attention_score = 10
@@ -92,40 +81,40 @@ def passed_fillers(items):
     return attention_score == 10, attention_score
 
 
-filtered_responses = []
-for w in responses:
-    test1 = w["positive_bias_average"] > w["negative_bias_average"]
-    test2, attention_score = passed_fillers(w["items"])
-    test1_text = "passed" if test1 else "failed"
-    test2_text = "passed" if test2 else "failed"
-    print("%s\t %s test1\t%s test2\tattention score=%s\t%s" % (w["WorkerId"], test1_text, test2_text, attention_score, w["Input.list"]))
-    if test1 and test2:
-        filtered_responses.append(w)
-
-items = {}
-for w in responses:
-    _, attention_score = passed_fillers(w["items"])
-    if attention_score < 6:
-        continue
-    for i in w["items"]:
-        if i["condition"] == "filler":
-            continue
-        if (i["item_number"], i["condition"]) not in items:
-            items[(i["item_number"], i["condition"])] = {
-                "responses": [],
-                "context": i["context"],
-                "prompt": i["prompt"]
-            }
-        items[(i["item_number"], i["condition"])]["responses"].append(i["response"])
-
-print("\t".join(["item number", "condition", "average", "length", "range", "standard deviation", "context", "prompt"]))
-for i in items:
-    length = len(items[i]["responses"])
-    average = np.mean(items[i]["responses"])
-    i_min = min(items[i]["responses"])
-    i_max = max(items[i]["responses"])
-    std = np.std(items[i]["responses"])
-    print("\t".join([i[0], i[1], str(average), str(length), str(i_max - i_min), str(std), items[i]["context"], items[i]["prompt"]]))
+# filtered_responses = []
+# for w in responses:
+#     test1 = w["positive_bias_average"] > w["negative_bias_average"]
+#     test2, attention_score = passed_fillers(w["items"])
+#     test1_text = "passed" if test1 else "failed"
+#     test2_text = "passed" if test2 else "failed"
+#     print("%s\t %s test1\t%s test2\tattention score=%s\t%s" % (w["WorkerId"], test1_text, test2_text, attention_score, w["Input.list"]))
+#     if test1 and test2:
+#         filtered_responses.append(w)
+#
+# items = {}
+# for w in responses:
+#     _, attention_score = passed_fillers(w["items"])
+#     if attention_score < 6:
+#         continue
+#     for i in w["items"]:
+#         if i["condition"] == "filler":
+#             continue
+#         if (i["item_number"], i["condition"]) not in items:
+#             items[(i["item_number"], i["condition"])] = {
+#                 "responses": [],
+#                 "context": i["context"],
+#                 "prompt": i["prompt"]
+#             }
+#         items[(i["item_number"], i["condition"])]["responses"].append(i["response"])
+#
+# print("\t".join(["item number", "condition", "average", "length", "range", "standard deviation", "context", "prompt"]))
+# for i in items:
+#     length = len(items[i]["responses"])
+#     average = np.mean(items[i]["responses"])
+#     i_min = min(items[i]["responses"])
+#     i_max = max(items[i]["responses"])
+#     std = np.std(items[i]["responses"])
+#     print("\t".join([i[0], i[1], str(average), str(length), str(i_max - i_min), str(std), items[i]["context"], items[i]["prompt"]]))
 
 
 
